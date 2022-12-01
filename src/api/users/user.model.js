70068dfa3af9d1324f3859878d07752c5be1693f
diego <diegomiller7@gmail.com>
2022-12-01 10:39:25 +0100

    FEAT: adding user model

---
+++ src/api/users/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true, trim: true }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', function(next) {
+  this.password = bcrypt.hashSync(this.password, 10); 
+  next();
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
