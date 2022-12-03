557c677ba45c0186f81f2fe218f37cb501c99dd9
Karuh13 <alvaro.c.s.13@gmail.com>
2022-12-03 22:30:22 +0100

    Added more products

--- src/utils/scrapper/genericProducts.js
+++ src/utils/scrapper/genericProducts.js
@@ -18,6 +18,222 @@ let genericProducts = [
             }
         ],
         supermarkets: []
+    },
+    {
+        name: "Filete de vacuno",
+        img: "pending",
+        category: "carnicería",
+        productCode: [
+            {
+                supermarketName: "carrefour",
+                supermarketCode: "R-prod1070081"
+            },
+            {
+                supermarketName: "dia",
+                supermarketCode: "181288"
+            },
+            {
+                supermarketName: "alcampo",
+                supermarketCode: "78424"
+            }
+        ],
+        supermarkets: []
+    },
+    {
+        name: "Filete de salmón",
+        img: "pending",
+        category: "pescadería",
+        productCode: [
+            {
+                supermarketName: "carrefour",
+                supermarketCode: "R-prod261161"
+            },
+            {
+                supermarketName: "dia",
+                supermarketCode: "162012"
+            },
+            {
+                supermarketName: "alcampo",
+                supermarketCode: "74418"
+            }
+        ],
+        supermarkets: []
+    },
+    {
+        name: "Filete de merluza",
+        img: "pending",
+        category: "pescadería",
+        productCode: [
+            {
+                supermarketName: "carrefour",
+                supermarketCode: "R-prod395352"
+            },
+            {
+                supermarketName: "dia",
+                supermarketCode: "192291"
+            },
+            {
+                supermarketName: "alcampo",
+                supermarketCode: "82345"
+            }
+        ],
+        supermarkets: []
+    },
+    {
+        name: "Botella de agua 1,5L",
+        img: "pending",
+        category: "bebidas",
+        productCode: [
+            {
+                supermarketName: "carrefour",
+                supermarketCode: "R-695701669"
+            },
+            {
+                supermarketName: "dia",
+                supermarketCode: "26995"
+            },
+            {
+                supermarketName: "alcampo",
+                supermarketCode: "32868"
+            }
+        ],
+        supermarkets: []
+    },
+    {
+        name: "Zumo de naranja con pulpa 1L",
+        img: "pending",
+        category: "bebidas",
+        productCode: [
+            {
+                supermarketName: "carrefour",
+                supermarketCode: "R-823705656"
+            },
+            {
+                supermarketName: "dia",
+                supermarketCode: "50690"
+            },
+            {
+                supermarketName: "alcampo",
+                supermarketCode: "71695"
+            }
+        ],
+        supermarkets: []
+    },
+    {
+        name: "Sandwich de nata 6 Ud.",
+        img: "pending",
+        category: "congelados",
+        productCode: [
+            {
+                supermarketName: "carrefour",
+                supermarketCode: "R-538003234"
+            },
+            {
+                supermarketName: "dia",
+                supermarketCode: "271108"
+            },
+            {
+                supermarketName: "alcampo",
+                supermarketCode: "67739"
+            }
+        ],
+        supermarkets: []
+    },
+    {
+        name: "Guisantes congelados 1Kg",
+        img: "pending",
+        category: "congelados",
+        productCode: [
+            {
+                supermarketName: "carrefour",
+                supermarketCode: "R-531705261"
+            },
+            {
+                supermarketName: "dia",
+                supermarketCode: "263266"
+            },
+            {
+                supermarketName: "alcampo",
+                supermarketCode: "69545"
+            }
+        ],
+        supermarkets: []
+    },
+    {
+        name: "Leche semidesnatada 1L",
+        img: "pending",
+        category: "lácteos",
+        productCode: [
+            {
+                supermarketName: "carrefour",
+                supermarketCode: "R-521007071"
+            },
+            {
+                supermarketName: "dia",
+                supermarketCode: "504"
+            },
+            {
+                supermarketName: "alcampo",
+                supermarketCode: "53549"
+            }
+        ],
+        supermarkets: []
+    },
+    {
+        name: "Mantequilla con sal 250g",
+        img: "pending",
+        category: "lácteos",
+        productCode: [
+            {
+                supermarketName: "carrefour",
+                supermarketCode: "R-fprod1350153"
+            },
+            {
+                supermarketName: "dia",
+                supermarketCode: "270690"
+            }
+        ],
+        supermarkets: []
+    },
+    {
+        name: "Pack macarrones 1Kg",
+        img: "pending",
+        category: "despensa",
+        productCode: [
+            {
+                supermarketName: "carrefour",
+                supermarketCode: "R-544501833"
+            },
+            {
+                supermarketName: "dia",
+                supermarketCode: "50973"
+            },
+            {
+                supermarketName: "alcampo",
+                supermarketCode: "27254"
+            }
+        ],
+        supermarkets: []
+    },
+    {
+        name: "Aceite de oliva virgen extra",
+        img: "pending",
+        category: "despensa",
+        productCode: [
+            {
+                supermarketName: "carrefour",
+                supermarketCode: "R-520660335"
+            },
+            {
+                supermarketName: "dia",
+                supermarketCode: "12360"
+            },
+            {
+                supermarketName: "alcampo",
+                supermarketCode: "29834"
+            }
+        ],
+        supermarkets: []
     }
 ]
 
