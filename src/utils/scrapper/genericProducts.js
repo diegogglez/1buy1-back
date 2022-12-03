c7fee200a2e0e7ca8bc4268c6a0a17536583b833
Karuh13 <alvaro.c.s.13@gmail.com>
2022-12-03 18:52:09 +0100

    FEAT: added product codes to genericProducts

--- src/utils/scrapper/genericProducts.js
+++ src/utils/scrapper/genericProducts.js
@@ -1,9 +1,24 @@
-[
+let genericProducts = [
     {
         name: "Pollo entero",
         img: "pending",
         category: "carnicería",
-        productLinks: [],
+        productCode: [
+            {
+                supermarketName: "carrefour",
+                supermarketCode: "R-791562917"
+            },
+            {
+                supermarketName: "dia",
+                supermarketCode: "6423"
+            },
+            {
+                supermarketName: "alcampo",
+                supermarketCode: "43298"
+            }
+        ],
         supermarkets: []
     }
 ]
+
+module.exports = genericProducts
